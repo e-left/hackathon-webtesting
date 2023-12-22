768ad80c6acb8bbbd96c1966c0f4667c54bec525
Fotis Alexandridis <fotisalexandridis2001@gmail.com>
2023-12-22 13:01:37 +0200

    test1

--- cypress/e2e/integration_tests/us1.cy.js
+++ cypress/e2e/integration_tests/us1.cy.js
@@ -1,39 +1,50 @@
 import Homepage from "../../page_object_model/homepage";
 
-describe('US1: Add Products in the Cart', () => {
+describe('US1', () => {
   const homepage = new Homepage();
   
-  before(() => {
+  beforeEach(() => {
     // visit home
-    const rootUrl = homepage.rootUrl;
-    cy.visit(rootUrl);
+    homepage.visitRoot();
   });
 
   it('View empty shopping chart', () => {
+    const homepage = new Homepage();
+
     // assert that the two numbers on top right (Items and Price) are zero
-    cy.get(".cart-info").children().get("strong").contains("0");
+    homepage.assertZeroItemsPrice();
 
     // click on the bag icon in the top right
-    cy.get('[alt="Cart"]').click();
+    homepage.clickOnCartBagButton();
 
     // ensure that user stays on the same page
-    const fullUrl = homepage.fullUrl;
-    cy.url().should("equal", `${fullUrl}#/`);
-
+    homepage.assertStayOnSamePage();
+    
     // ensure that shopping cart pops up under bag icon
-    cy.get(".cart-preview").should("have.class", "active");
+    homepage.assertCartHasAppeared();
 
     // ensure that the message "Your cart is empty!" is displayed
-    cy.get(".cart-preview").contains("Your cart is empty!");
+    homepage.assertEmptyCartMessageAppears();
 
     // ensure that the PROCEED TO CHECKOUT button is disabled
-
-    // ensure that the PROCEED TO CHECKOUT button is a light orange color
+    homepage.assertProceedToCheckoutButtonIsDisabled();
 
     // click on the bag icon again
-    cy.get('[alt="Cart"]').click();
+    homepage.clickOnCartBagButton();
 
     // ensure shopping cart is hidden
+    homepage.assertCartIsHidden();
+  });
 
-  })
+  // it("Checkout an empty shopping cart", () => {
+
+  // });
+
+  // it("Add single product to cart multiple times", () => {
+
+  // });
+
+  // it("Add multiple distinct items to cart", () => {
+
+  // });
 });
--- cypress/page_object_model/homepage.js
+++ cypress/page_object_model/homepage.js
@@ -1,14 +1,86 @@
 class Homepage {
-    constructor() {
-
-    }
-
     get fullUrl() {
         return 'https://rahulshettyacademy.com/seleniumPractise/';
     }
+    get cartInfoPanel() {
+        return ".cart-info";
+    }
 
-    get rootUrl() {
-        return "/";
+    get cartBagButton() {
+        return '[alt="Cart"]';
+    }
+
+    get activeClass() {
+        return 'active';
+    }
+
+    get strongElement() {
+        return "strong";
+    }
+
+    get emptyItemsData() {
+        return "0";
+    }
+
+    get cartPanel() {
+        return ".cart-preview";
+    }
+
+    get emptyCartPanel() {
+        return ".empty-cart";
+    }
+
+    get h2Element() {
+        return "h2";
+    }
+
+    get emptyCartData() {
+        return "You cart is empty!";
+    }
+
+    get buttonElement() {
+        return "button";
+    }
+
+    get actionBlockClass() {
+        return ".action-block";
+    }
+
+    get disabledClass() {
+        return "disabled";
+    }
+
+    visitRoot() {
+        return cy.visit("/");
+    }
+
+    assertZeroItemsPrice() {
+        return cy.get(this.cartInfoPanel).children().get(this.strongElement).contains(this.emptyItemsData);
+    }
+
+    clickOnCartBagButton() {
+        return cy.get(this.cartBagButton).click();
+    }
+
+    assertStayOnSamePage() {
+        return cy.url().should("equal", `${this.fullUrl}#/`);
+    }
+
+    assertCartHasAppeared() {
+        return cy.get(this.cartPanel).should("have.class", this.activeClass);
+    }
+
+    assertEmptyCartMessageAppears() {
+        cy.get(this.emptyCartPanel).should('exist');
+        return cy.get(this.emptyCartPanel).get(this.h2Element).contains(this.emptyCartData);
+    }
+
+    assertProceedToCheckoutButtonIsDisabled() {
+        return cy.get(this.cartPanel).get(this.actionBlockClass).get(this.buttonElement).should("have.class", this.disabledClass);
+    }
+
+    assertCartIsHidden() {
+        return cy.get(this.cartPanel).should("not.have.class", this.activeClass);
     }
 
 }
