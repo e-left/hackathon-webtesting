3ca0ceb0bdf6a362f7bce26cb21b32c2b5fb5068
panoschal <27917356+panoschal@users.noreply.github.com>
2023-12-22 14:44:18 +0200

    increase timeout

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   projectId: "nugb51",
   e2e: {
     baseUrl: 'https://rahulshettyacademy.com/seleniumPractise/',
+    defaultCommandTimeout: 6000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
