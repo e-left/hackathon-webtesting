64994cda6c3b93d0daee89fb0fccd11125a33f95
Fotis Alexandridis <fotisalexandridis2001@gmail.com>
2023-12-22 11:51:30 +0200

    added base url

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "nugb51",
   e2e: {
+    baseUrl: 'https://rahulshettyacademy.com/seleniumPractise/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/page_object_model/homepage.js
+++ cypress/page_object_model/homepage.js
@@ -1,4 +1,7 @@
 class Homepage {
+    constructor() {
+
+    }
 
 }
 
