57a41be31a588fc7e5049bf183a44af7b0f7cfea
panoschal <27917356+panoschal@users.noreply.github.com>
2023-12-22 14:43:49 +0200

    add us2

--- cypress/e2e/integration_tests/us2.cy.js
+++ cypress/e2e/integration_tests/us2.cy.js
@@ -1,11 +1,21 @@
 import CartPage from '../../page_object_model/cartpage';
+import Homepage from '../../page_object_model/homepage';
 
 describe('Use Promo Codes/Coupons for an Order', () => {
   beforeEach(() => {
-    // visit the home page
-    cy.visit('/');
+    const homepage = new Homepage();
 
-    // select brocolli and cauliflower
+    // visit the homepage
+    homepage.visitRoot();
+
+    // add 2 brocolli and 1 cauliflower
+    homepage.addSingleProductToCart('Brocolli', 1);
+    homepage.addSingleProductToCart('Cauliflower', 0);
+
+    homepage.clickOnCartBagButton();
+    homepage.clickProceedToCheckoutButton();
+
+    cy.url().should('contain', 'cart');
   });
 
   it('test1', () => {
@@ -13,5 +23,20 @@ describe('Use Promo Codes/Coupons for an Order', () => {
     page.checkItemQuantity('Brocolli - 1 Kg', '2');
     page.checkItemPrice('Brocolli - 1 Kg', '120');
     page.checkItemTotal('Brocolli - 1 Kg', '240');
+
+    page.checkItemQuantity('Cauliflower - 1 Kg', '1');
+    page.checkItemPrice('Cauliflower - 1 Kg', '60');
+    page.checkItemTotal('Cauliflower - 1 Kg', '60');
+  });
+
+  it('2', () => {
+    const page = new CartPage();
+    page.checkTableColumnNames();
+  })
+
+  it('3', () => {
+    const page = new CartPage();
+    page.clickPlaceOrder();
+    // 
   })
 });
